067f05bb185b4ed0e668bbffbe7987c6e2ce9c0b
MLET-007 <165738023+MLET-007@users.noreply.github.com>
2024-09-23 22:34:31 -0400

    Update bc1378e4e526_init_db.py

--- alembic/versions/bc1378e4e526_init_db.py
+++ alembic/versions/bc1378e4e526_init_db.py
@@ -19,18 +19,35 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     op.create_table(
-        "processos",
-        sa.Column("id", sa.Integer, primary_key=True),
-        sa.Column("numero_processo", sa.String(50), nullable=False),
+        "processo",
+        #sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("numero_processo", sa.String(100), nullable=False, primary_key=True),
         sa.Column("tribunal", sa.String(100), nullable=False),
         sa.Column("grau", sa.String(100), nullable=False),
-        sa.Column("data_juizamento", sa.DateTime, nullable=False),
-        sa.Column("movimentacoes", sa.String(100), nullable=False),
-        sa.Column("assuntos", sa.String(100), nullable=False),
-        sa.Column("codigo_da_classe", sa.String(100), nullable=False),
-        sa.Column("codigo_formato", sa.String(100), nullable=False),
+        sa.Column("orgao_codigo", sa.Float, nullable=False),
+        sa.Column("orgao_nome", sa.String(100), nullable=False),
+
+        sa.Column("data_ajuizamento", sa.DateTime, nullable=False),
+        sa.Column("data_julgamento", sa.DateTime, nullable=False),
+        sa.Column("tempo_julgamento", sa.Float, nullable=False),
+
+        sa.Column("codigo_classe", sa.Float, nullable=False),
+        sa.Column("classe_nivel1", sa.Float, nullable=False),
+        sa.Column("classe_nivel2", sa.Float, nullable=True),
+        sa.Column("classe_nivel3", sa.Float, nullable=True),
+        sa.Column("classe_nivel4", sa.Float, nullable=True),
+        sa.Column("classe_nivel5", sa.Float, nullable=True),
+
+        sa.Column("codigo_assunto", sa.Float, nullable=False),
+        sa.Column("assunto_nivel1", sa.Float, nullable=False),
+        sa.Column("assunto_nivel2", sa.Float, nullable=True),
+        sa.Column("assunto_nivel3", sa.Float, nullable=True),
+        sa.Column("assunto_nivel4", sa.Float, nullable=True),
+        sa.Column("assunto_nivel5", sa.Float, nullable=True),
+
+        sa.Column("codigo_formato", sa.Float, nullable=False)
     )
 
 
 def downgrade() -> None:
-    op.drop_table("processos")
+    op.drop_table("processo")
